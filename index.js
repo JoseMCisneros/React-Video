7c10cdb1a7a8598d68292a4b31e06ab709ddd20a
Jose <j.cisneros1195@gmail.com>
2019-01-21 21:51:16 -0600

    Creando Componentes ReactJS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
 
 // console.log('Hola mundo!');
 
 const app = document.getElementById('app');
 
 // ReactDOM.render(que voy a renderizar, donde lo hare);
-const holaMundo = <h1>Hola Estudiante!</h1>;
-ReactDOM.render(holaMundo, app);
+// const holaMundo = <h1>Hola Estudiante!</h1>;
+
+render(<Media />, app);
