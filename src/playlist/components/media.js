77b33507425e1da34525dabfc6cf07402de41a64
Jose <j.cisneros1195@gmail.com>
2019-01-22 19:06:08 -0600

    Estado de los componentes en ReactJS

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -3,12 +3,21 @@ import './media.css';
 import PropTypes from 'prop-types';
 
 class Media extends Component{
+  state = {
+    author: 'Jose Manuel'
+  }
   // constructor(props){
   //   super(props)
-  //   this.handleClick = this.handleClick.bind(this);
+  //   this.state = {
+  //     author: props.author
+  //   }
+  //   // this.handleClick = this.handleClick.bind(this);
   // }
   handleClick = (event) =>{
-    console.log(this.props.image);
+    // console.log(this.props.image);
+    this.setState({
+      author: 'Ricardo Celis'
+    })
   }
 
   render(){
@@ -23,7 +32,7 @@ class Media extends Component{
             height={160}
           />
           <h3 className="Media-title">{this.props.title}</h3>
-          <p className="Media-author">{this.props.author}</p>
+          <p className="Media-author">{this.state.author}</p>
         </div>
       </div>
     )
