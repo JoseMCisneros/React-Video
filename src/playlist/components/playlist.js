45942f1c9f51e057756d5143aae966335d43634e
Jose <j.cisneros1195@gmail.com>
2019-01-25 22:38:03 -0600

    Componentes puros y funcionales en ReactJS

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
-import Media from './media';
-import './playlist.css'
+import React from 'react';
+import Media from './media.js';
+import './playlist.css';
 
-class Playlist extends Component{
-  render(){
-    const playlist = this.props.data.categories[0].playlist;
-    console.log(this.props.data);
+// function Playlist(props){
+//   return(
+//     <div onClick={props.handleClick}>
+//       {props}
+//     </div>
+//   )
+// }
+
+function Playlist(props){
+    const playlist = props.data.categories[0].playlist;
+    console.log(props.data);
     return(
       <div className="Playlist">
         {
@@ -16,6 +23,5 @@ class Playlist extends Component{
       </div>
     )
   }
-}
 
 export default Playlist;
