fa367108549b6ff86912a128c25d19b19ef3321d
Jose <j.cisneros1195@gmail.com>
2019-02-06 16:27:07 -0600

    UI de Formularios

--- src/categories/components/categories.js
+++ src/categories/components/categories.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Category from './category'
+import Category from './category';
 import './categories.css';
+import Search from '../../widgets/containers/search';
 
 function Categories(props) {
   return(
     <div className="Categories">
+    <Search />
       {
         props.categories.map((item) => {
           return (
@@ -20,4 +22,4 @@ function Categories(props) {
   )
 }
 
-export default Categories
+export default Categories;
